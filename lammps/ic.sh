4a69a6226ebdfb6b525eb7573e043f59f890a0ff
Hao Zhao <49770714+haoxy97@users.noreply.github.com>
2024-03-07 10:34:21 +0000

    Update ic.sh, simple version

--- lammps/ic.sh
+++ lammps/ic.sh
@@ -6,60 +6,21 @@ module purge
 module load tools/eb-dev
 module load libreadline
 module load aocc/2.3.0
-des=${HOME}/software
 
-mkdir -p ${des}/openmpi4
-mkdir -p ${des}/lammps
-mkdir -p ${des}/netcdf
+cd ${HOME}
+mkdir -p ${HOME}/openmpi4
+mkdir -p ${HOME}/lammps
 
 cd ${HOME}
 wget 'https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.5.tar.gz'
 tar -xvf openmpi-4.1.5.tar.gz
 cd openmpi-4.1.5
-./configure CC=clang CCX=clang++ FC=flang --prefix=${des}/openmpi4 --enable-orterun-prefix-by-default --enable-mpi-cxx
+./configure CC=clang CCX=clang++ FC=flang --prefix=${HOME}/openmpi4 --enable-orterun-prefix-by-default --enable-mpi-cxx
 make -j 8
 make install
-export PATH="${des}/openmpi4/bin:$PATH"
-export LD_LIBRARY_PATH="${des}/openmpi4/lib:$LD_LIBRARY_PATH"
+export PATH="${HOME}/openmpi4/bin:$PATH"
+export LD_LIBRARY_PATH="${HOME}/openmpi4/lib:$LD_LIBRARY_PATH"
 
-cd ${HOME}
-val=sz2
-wget 'https://support.hdfgroup.org/ftp/lib-external/szip/2.1.1/src/szip-2.1.1.tar.gz'
-mkdir -p ${val}-stable
-tar -xvf szip-2.1.1.tar.gz -C ${val}-stable --strip-components=1
-cd ${val}-stable
-./configure CC=clang CCX=clang++ FC=flang --prefix=${des}/${val}
-make -j 8
-make install
-cd ${HOME}/${val}/lib
-export LD_LIBRARY_PATH="${des}/sz2/lib:$LD_LIBRARY_PATH"
-
-cd ${HOME}
-val=hdf5
-wget 'https://github.com/HDFGroup/hdf5/releases/download/hdf5-1_14_2/hdf5-1_14_2.tar.gz'
-mkdir -p ${val}-stable
-tar -xvf hdf5-1_14_2.tar.gz -C ${val}-stable --strip-components=1
-cd ${val}-stable/hdfsrc
-./configure CC=clang CCX=clang++ FC=flang --prefix=${des}/${val}
-make -j 8
-make install
-cd ${HOME}/${val}/lib
-ln -s libhdf5_hl.so libhdf5_hl.so.100
-ln -s libhdf5.so libhdf5.so.103
-export LD_LIBRARY_PATH="${des}/hdf5/lib:$LD_LIBRARY_PATH"
-
-cd ${HOME}
-val=netcdf
-wget 'https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz'
-mkdir -p netcdf-stable
-tar -xvf netcdf-c-4.9.2.tar.gz -C netcdf-stable --strip-components=1
-cd netcdf-stable
-./configure CC=clang CCX=clang++ FC=flang --prefix=${des}/${val}
-make -j 8
-make install
-cd ${des}/${val}/lib
-ln -s libnetcdf.so libnetcdf.so.15
-export LD_LIBRARY_PATH="${des}/netcdf/lib:$LD_LIBRARY_PATH"
 
 cd ${HOME}
 wget 'https://download.lammps.org/tars/lammps-stable.tar.gz'
@@ -69,18 +30,14 @@ cd lammps-stable
 mkdir build
 cd build
 
-cmake -DCMAKE_PREFIX_PATH=${des}/openmpi4/lib   CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_Fortran_COMPILER=flang\
- -D PKG_OPENMP=yes -D PKG_OPT=yes -D PKG_NETCDF=yes -D PKG_REAXFF=yes -D PKG_EXTRA-FIX=yes\
- -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes  -D LAMMPS_EXCEPTIONS=yes  -D BUILD_MPI=yes\
- -D CMAKE_INSTALL_PREFIX=${des}/lammps ../cmake
+cmake -D PKG_OPENMP=yes -D PKG_OPT=yes -D BUILD_LAMMPS_SHELL=yes  -D LAMMPS_EXCEPTIONS=yes  -D BUILD_MPI=yes\
+ -D CMAKE_INSTALL_PREFIX=${HOME}/lammps ../cmake
 make -j 8
 make install
 
-export PATH="${HOME}/software/lammps/bin:$PATH"
+export PATH="${HOME}/lammps/bin:$PATH"
 
 echo try input lmp to test it.
-
-
 echo example of your pbs.sh file
 echo '''------------------------------------
 #!/bin/bash
@@ -93,8 +50,9 @@ module purge
 module load aocc/2.3.0
 
 export OMP_NUM_THREADS=1 MKL_NUM_THREADS=1
-export PATH="${HOME}/software/lammps/bin:${HOME}/software/openmpi4/bin:$PATH"
-export LD_LIBRARY_PATH="${HOME}/software/openmpi4/lib:${HOME}/software/netcdf/lib:${HOME}/software/hdf5/lib:${HOME}/software/sz2/lib:$LD_LIBRARY_PATH"
+export PATH="${HOME}/lammps/bin:${HOME}/openmpi4/bin:$PATH"
+export LD_LIBRARY_PATH="${HOME}/openmpi4/lib:${HOME}/netcdf/lib:${HOME}/hdf5/lib:${HOME}/sz2/lib:$LD_LIBRARY_PATH"
 
-mpirun -np 16 lmp -i lmp.sh
+mpirun -np 4 lmp -i lmp.sh
 ------------------------------------'''
+
