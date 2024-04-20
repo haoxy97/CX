e941fec6b33942323203340d7a338213f13cb3aa
Hao Zhao <49770714+haoxy97@users.noreply.github.com>
2024-04-20 17:52:45 +0100

    Update z2.sh

--- lammps/z2.sh
+++ lammps/z2.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
-sudo apt install build-essential cmake ninja-build python3-distutils python3-apt\ 
-cmake ffmpeg libnetcdf-dev git netcdf-bin  libreadline-dev libnuma-dev -y 
 
+# install intel one api
+#source ${HOME}/intel/oneapi/setvars.sh
+# aocc
+# source ${HOME}/software/setenv_AOCC.sh
+# cuda
+# export PATH="$PATH:/usr/local/cuda-12.4/bin"
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-12.4/lib64"
+
+sudo apt install build-essential cmake ninja-build python3-distutils python3-apt cmake ffmpeg libnetcdf-dev git netcdf-bin gfortran  libreadline-dev libnuma-dev -y 
 cd
 mkdir $HOME/software
 sudo apt install build-essential cmake ninja-build python3-distutils python3-apt
@@ -23,35 +30,26 @@ export LD_LIBRARY_PATH="${HOME}/software/clang/lib:$LD_LIBRARY_PATH"
 cd $HOME/software
 mkdir openmpi4 lammps
 
-# install intel one api
-source ${HOME}/intel/oneapi/setvars.sh
-# aocc
-source ${HOME}/software/setenv_AOCC.sh
-
-wget 'https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.5.tar.gz'
-tar -xvf openmpi-4.1.5.tar.gz
-cd openmpi-4.1.5/
-./configure CC=clang CCX=clang++ FC=flang --prefix=${HOME}/openmpi4 --enable-orterun-prefix-by-default --enable-mpi-cxx
-make -j 8
+wget 'https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.6.tar.gz'
+tar -xvf openmpi-4.1.6.tar.gz
+cd openmpi-4.1.6/
+./configure CC=clang CCX=clang++ CFLAGS="-mavx2" CXXFLAGS="-mavx2" --prefix=${HOME}/software/openmpi4 --enable-orterun-prefix-by-default --enable-mpi-cxx
+make -j
 make install
-export PATH="${HOME}/openmpi4/bin:$PATH"
-export LD_LIBRARY_PATH="${HOME}/openmpi4/lib:$LD_LIBRARY_PATH"
-# cuda
-export PATH="$PATH:/usr/local/cuda-12.4/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-12.4/lib64"
+export PATH="${HOME}/software/openmpi4/bin:$PATH"
+export LD_LIBRARY_PATH="${HOME}/software/openmpi4/lib:$LD_LIBRARY_PATH"
 
-cd ~
+cd ${HOME}/software
 wget 'https://download.lammps.org/tars/lammps-stable.tar.gz'
 mkdir -p lammps-stable
 tar -xvf lammps-stable.tar.gz -C lammps-stable --strip-components=1
 cd lammps-stable
 mkdir build
 cd build
-cmake -D CMAKE_CXX_COMPILER=icx -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D PKG_REAXFF=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes -D PKG_VORONOI=yes -D DOWNLOAD_VORO=yes -D PKG_OPENMP=yes -D BUILD_OMP=yes -D PKG_NETCDF=yes PKG_OPT=yes  -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D CMAKE_INSTALL_PREFIX=${HOME}/lammps ../cmake
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D PKG_REAXFF=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes -D PKG_VORONOI=yes -D DOWNLOAD_VORO=yes -D PKG_OPENMP=yes -D BUILD_OMP=yes -D PKG_NETCDF=yes PKG_OPT=yes  -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D CMAKE_INSTALL_PREFIX=${HOME}/software/lammps ../cmake
 # cuda version not use icc
-# cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=single  -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D PKG_REAXFF=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes   -D PKG_OPENMP=yes -D BUILD_OMP=yes -D PKG_NETCDF=yes PKG_OPT=yes  -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D CMAKE_INSTALL_PREFIX=${HOME}/lammps ../cmake
-
-make -j 8
+# cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=single  -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D PKG_REAXFF=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes   -D PKG_OPENMP=yes -D BUILD_OMP=yes -D PKG_NETCDF=yes PKG_OPT=yes  -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D CMAKE_INSTALL_PREFIX=${HOME}/software/lammps ../cmake
+make -j
 make install
 
 export PATH="${HOME}/lammps/bin:$PATH"
