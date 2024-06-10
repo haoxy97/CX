26c0a1c61de955cb0562f4b04093d2c58905a8ee
Hao Zhao <49770714+haoxy97@users.noreply.github.com>
2024-06-10 12:49:14 +0100

    Update Mac.sh

--- lammps/Mac.sh
+++ lammps/Mac.sh
@@ -9,13 +9,15 @@ tar -xvf lammps-stable.tar.gz -C lammps-stable --strip-components=1
 cd lammps-stable
 mkdir build; cd build
 
+export OMP_PREFIX=/opt/homebrew/opt/libomp
+export LDFLAGS="-L/opt/homebrew/opt/libomp/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libomp/include"
+echo "need change the right version of libomp lib like "18.1.7""
 export PKG_CONFIG_PATH="/opt/homebrew/opt/readline/lib/pkgconfig"
-cmake -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ \
--D BUILD_OMP=yes  -D PKG_OPENMP=yes -D PKG_OPT=yes -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D PKG_NETCDF=yes \
--D PKG_REAXFF=yes -D PKG_OPT=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes -D PKG_VORONOI=yes \
--D DOWNLOAD_VORO=yes -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp /opt/homebrew/opt/libomp/lib/libomp.dylib -I/opt/homebrew/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES="libomp"\
--DOpenMP_libomp_LIBRARY="/opt/homebrew/Cellar/libomp/17.0.2/lib/libomp.dylib"  -DCMAKE_INSTALL_PREFIX=/opt/lammps ../cmake
-make -j 8
+cmake -D BUILD_TOOLS=yes -D BUILD_LAMMPS_SHELL=yes -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++  -D BUILD_OMP=yes  -D PKG_OPENMP=yes -D PKG_OPT=yes -D LAMMPS_EXCEPTIONS=yes -D BUILD_MPI=yes -D PKG_NETCDF=yes -D PKG_REAXFF=yes -D PKG_OPT=yes -D PKG_EXTRA-FIX=yes -D PKG_EXTRA-COMPUTE=yes -D PKG_H5MD=yes -D PKG_VORONOI=yes -D DOWNLOAD_VORO=yes -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp /opt/homebrew/opt/libomp/lib/libomp.dylib -I/opt/homebrew/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES="libomp" -DOpenMP_libomp_LIBRARY="/opt/homebrew/Cellar/libomp/18.1.7/lib/libomp.dylib"  -DCMAKE_INSTALL_PREFIX=/opt/lammps ../cmake
+
+make -j 4
 make install
+
 cd ~
 echo 'export PATH="/opt/lammps/bin:$PATH"' >> .zshrc
